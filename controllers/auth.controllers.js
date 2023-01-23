9b551571044fdb81fb41872032eb5f7016e5f076
Rennan <reiony.dev@gmail.com>
2023-01-23 05:28:30 -0300

    feat: auth controllers

---
+++ controllers/auth.controllers.js
@@ -0,0 +1,33 @@
+import { usersCollection, sessionsCollection} from "../database/database.js";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+export async function signUp(req, res) {
+  const user = res.locals.user;
+  const passwordHashed = bcrypt.hashSync(user.password, 10);
+
+  try {
+    await usersCollection.insertOne({ ...user, password: passwordHashed });
+    res.status(201).send("User created successfully");
+    return;
+  } catch (err) {
+    const errors = err.details.map((detail) => detail.message);
+    res.status(400).send(errors);
+    return;
+  }
+}
+
+export async function signIn(req, res) {
+    const user = res.locals.user;
+    const token = uuidV4();
+  
+    try {
+      await sessionsCollection.insertOne({ token, userId: user._id });
+      res.status(201).send("User Logged In successfully");
+      return;
+    } catch (err) {
+      const errors = err.details.map((detail) => detail.message);
+      res.status(400).send(errors);
+      return;
+    }
+  }
