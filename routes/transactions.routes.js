2ab64a5a80e613268f42f7122bd63f020beb4ea9
Rennan <reiony.dev@gmail.com>
2023-01-23 21:14:00 -0300

    fix: transactionsSchema on transactions routes

--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getTransaction, registerTransaction } from "../controllers/transactions.controllers.js";
+import {getTransactions, registerTransactions } from "../controllers/transactions.controllers.js";
 import { authValidationToken } from "../middlewares/authValidation.middleware.js";
+import { transactionsSchemaValidation } from "../middlewares/transactions.middleware.js";
 
 const router = Router();
+
 router.use(authValidationToken);
-router.post("/transactions", registerTransaction);
-router.get("/transactions", getTransaction);
+
+router.post("/transactions", transactionsSchemaValidation, registerTransactions);
+router.get("/transactions", getTransactions);
+
 export default router;
